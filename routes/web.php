ea78340c7e089c6edbfd955d8ada683b146f941f
Hector <hectorvallsmira2003@gmail.com>
2024-02-12 18:37:45 +0100

    Registro con gitgub

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,11 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CiclosController;
 use App\Http\Controllers\ActivateUserThingsController;
+use App\Models\User;
+use App\Models\Alumnos;
+use Illuminate\Support\Facades\Hash;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -39,3 +44,42 @@ Route::get('/ofertas/activar/{id}/', [ActivateUserThingsController::class, 'vali
 
 Route::resource('ciclos', CiclosController::class);
 
+
+Route::get('/auth/github/redirect', function (){
+    return Socialite::driver('github')->redirect();
+
+});
+Route::get('/auth/github/callback', function () {
+    $githubUser = Socialite::driver('github')->user();
+
+    $admin = User::where('rol', 'administrador')->first();
+            // Crear usuario
+            $user = new User();
+            $user->name = $githubUser->name;
+            $user->email = $githubUser->email;
+            $user->password = Hash::make("password");
+            $user->direccion = "";
+            $user->rol = 'alumno';
+            $user->save();
+            // Crear el alumno asociada al usuario
+            $alumno = new Alumnos();
+            $alumno->apellido = "";
+            $alumno->cv = $githubUser->user['html_url'];
+
+            // Guardar el alumno asociada al usuario
+            $user->alumno()->save($alumno);
+            $alumno = Alumnos::findOrFail($user->id);
+
+            $user->notify(new ActivarCuentaNotification($user));
+
+
+            foreach ($request->ciclosA as $cicloA) {
+                $ciclo = Ciclos::findOrFail($cicloA['id']);
+                $alumno->ciclos()->attach($ciclo->id, [
+                    'finalizacion' => $cicloA['finalizacion'],
+                ]);                
+                $ciclo->usuarioResponsable->notify(new ValidarCiclosNotification($alumno, $ciclo));
+                $admin->notify(new ValidarCiclosNotification($alumno, $ciclo));
+
+            }
+});
